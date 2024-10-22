53ce267b5cf34b76eca14634b1b00cf7a0bf7efc
Lucas Martins <lucas.martins@masonweb.com.br>
2024-10-22 03:15:31 -0300

    Fix pyenv

--- include/15-exportapps.zsh
+++ include/15-exportapps.zsh
@@ -4,6 +4,5 @@ export PATH=$PATH:$(go env GOPATH)/bin
 export PYENV_ROOT="$HOME/.pyenv"
 command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
 export PIPENV_VENV_IN_PROJECT=1
 export PIPENV_SHELL=zsh
