7b4a3f03ebe316a64ee7f0e88c6db4143de6f2e3
Lucas Martins <lucas.martins@masonweb.com.br>
2024-11-06 11:53:00 -0300

    Retirando plugin vscode macOS.

--- include/20-plugins.zsh
+++ include/20-plugins.zsh
@@ -1,2 +1,2 @@
 ### Config Plugins
-plugins=( golang macos git tmux vscode vagrant node npm poetry docker docker-compose brew aws terraform helm ansible zsh-syntax-highlighting zsh-colorls zsh-nvm )
+plugins=( golang macos git tmux vagrant node npm poetry docker docker-compose brew aws terraform helm ansible zsh-syntax-highlighting zsh-colorls zsh-nvm )
